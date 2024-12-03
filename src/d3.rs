435c2df4d26b18020584cd005415339ec6351c74
hre <hre@orgadata.ag>
2024-12-03 10:03:32 +0100

    Refactor day 3 logic to streamline operation handling and improve readability

--- src/d3.rs
+++ src/d3.rs
@@ -3,51 +3,34 @@ use std::io::{self, BufRead};
 use regex::Regex;
 
 pub(crate) fn run() {
-    let file = File::open("d3-input.txt").expect("Failed to open input.txt");
+    let file = File::open("d3-input.txt").expect("Failed to open d3-input.txt");
     let reader = io::BufReader::new(file);
 
-    let mut operations: Vec<String> = Vec::new();
+    let mut result: u32 = 0;
     let mut operations_enabled = true;
-    
+
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
-        let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
 
         for cap in re.captures_iter(&line) {
-            let cap_str = cap.get(0).unwrap().as_str();
-            if cap_str == "do()" {
-                operations_enabled = true;
-                continue;
-            } else if cap_str == "don't()" {
-                operations_enabled = false;
-                continue;
+            if let Some(a_match) = cap.get(1) {
+                if operations_enabled {
+                    let a: u32 = a_match.as_str().parse().expect("Failed to parse number");
+                    let b: u32 = cap[2].parse().expect("Failed to parse number");
+                    result += a * b;
+                }
+            } else {
+                match cap.get(0).unwrap().as_str() {
+                    "do()" => operations_enabled = true,
+                    "don't()" => operations_enabled = false,
+                    _ => {}
+                }
             }
-
-            if !operations_enabled {
-                continue;
-            }
-
-            operations.push(cap_str.to_string());
         }
     }
 
     println!("Day 3:");
-
-    let mut result: u32 = 0;
-
-    for operation in operations {
-        let nums = operation
-            .split(|c| c == '(' || c == ')' || c == ',')
-            .filter(|s| !s.is_empty())
-            .skip(1)
-            .map(|s| s.parse().expect("Failed to parse number"))
-            .collect::<Vec<u32>>();
-        result += mul(nums[0], nums[1]);
-    }
-
     println!("{}", result);
-}
-
-fn mul(a: u32, b: u32) -> u32 {
-    a * b
 }
